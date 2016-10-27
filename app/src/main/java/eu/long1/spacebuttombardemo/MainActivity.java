e32f60777be85310488ecb2b40148ef42b9849ca
Razvan Cristian Lung <lung.razvan@gmail.com>
2016-10-28 02:35:28 +0300

    done it but it is static

--- app/src/main/java/eu/long1/spacebuttombardemo/MainActivity.java
+++ app/src/main/java/eu/long1/spacebuttombardemo/MainActivity.java
@@ -11,8 +11,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        SpaceButtonView spaceButton = (SpaceButtonView) findViewById(R.id.spaceButton);
-        spaceButton.setButtonImage(R.drawable.ic_fail);
     }
 }
--- spacebuttonbar/src/main/java/eu/long1/spacebuttonbar/SpaceButtonView.java
+++ spacebuttonbar/src/main/java/eu/long1/spacebuttonbar/SpaceButtonView.java
@@ -3,13 +3,12 @@ package eu.long1.spacebuttonbar;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.os.Build;
-import android.support.annotation.ColorRes;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.RequiresApi;
+import android.support.design.widget.FloatingActionButton;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -17,9 +16,12 @@ import android.widget.TextView;
 
 public class SpaceButtonView extends LinearLayout {
 
-    TextView actionLeft;
-    ImageView button;
-    TextView actionRight;
+
+    TextView actionLeftText;
+    ImageView actionLeftImage;
+    FloatingActionButton button;
+    TextView actionRightText;
+    ImageView actionRightImage;
 
     public SpaceButtonView(Context context) {
         super(context);
@@ -30,7 +32,7 @@ public class SpaceButtonView extends LinearLayout {
         super(context, attrs);
 
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.SpaceButtonView, 0, 0);
-        int buttonIcon = a.getResourceId(R.styleable.SpaceButtonView_icon, R.drawable.ic_fail);
+        int buttonIcon = a.getResourceId(R.styleable.SpaceButtonView_icon, R.drawable.ic_button);
         a.recycle();
 
         setOrientation(LinearLayout.HORIZONTAL);
@@ -42,11 +44,15 @@ public class SpaceButtonView extends LinearLayout {
         inflater.inflate(R.layout.space_button_layout, this, true);
 
 
-        actionLeft = (TextView) getChildAt(0);
-        button = (ImageView) getChildAt(1);
-        actionRight = (TextView) getChildAt(2);
 
-        button.setImageResource(buttonIcon);
+
+        //actionLeft = (TextView) ((LinearLayout) getChildAt(0)).getChildAt(0);
+        //button = (ImageView) mainLayout.getChildAt(1);
+        //actionRight = (TextView) getChildAt(2);
+
+        // button.setImageResource(buttonIcon);
+
+        setBackground(getResources().getDrawable(R.drawable.background));
     }
 
     public SpaceButtonView(Context context, AttributeSet attrs, int defStyleAttr) {
